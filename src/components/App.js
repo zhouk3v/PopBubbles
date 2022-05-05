d443fe450b1ad5febed93ff21d1a9eb11df756b2
DESKTOP-FPSVATK\zhouk <zhoukev@hotmail.com>
2022-05-04 20:58:20 -0400

    Gettign initial token from PKCE path

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import crypto from "crypto";
 
 import BubblesList from "./BubblesList";
 import "./css/App.css";
@@ -7,31 +8,99 @@ export const authEndpoint = "https://accounts.spotify.com/authorize";
 
 const clientId = "ffa134402f974b84b32367f0e4a7babb";
 // Switch to localhost when testing locally
-//const redirectUri = "http://localhost:3000/";
-const redirectUri = "https://popbubbles.netlify.app";
+const redirectUri = "http://localhost:3000/";
+//const redirectUri = "https://popbubbles.netlify.app";
 const scopes = ["user-top-read"];
 
-const hash = window.location.hash
-  .substring(1)
-  .split("&")
-  .reduce(function (initial, item) {
-    if (item) {
-      var parts = item.split("=");
-      initial[parts[0]] = decodeURIComponent(parts[1]);
-    }
-    return initial;
-  }, {});
-
-window.location.hash = "";
-
 class App extends React.Component {
-  state = { token: null };
-  // Note that the component unmounts when the user clicks login into spotify, then remounts after logging in
+  constructor() {
+    super();
+    this.token = localStorage.getItem("accesstoken");
+    this.state = {
+      tokenExists: this.token ? true : false,
+      stateValid: false,
+      urlState: null,
+      codeChallenge: null,
+      validState: true,
+    };
+  }
+
   componentDidMount() {
-    let token = hash.access_token;
-    if (token) {
+    if (this.token === null) {
+      // Start the process of requesting a new token through PKCE authorization flow
+      this.handleRedirect();
+    } else {
+      // If token is expired, renew it
+      this.setState({ tokenExists: true });
+    }
+  }
+
+  base64Encode(str) {
+    return str
+      .toString("base64")
+      .replace(/\+/g, "-")
+      .replace(/\//g, "_")
+      .replace(/=/g, "");
+  }
+
+  sha256(buffer) {
+    return crypto.createHash("sha256").update(buffer).digest();
+  }
+
+  async generateNewToken(code) {
+    // Grab the code verifier from session storage
+    const codeVerifier = sessionStorage.getItem("codeVerifier");
+    // Send a POST request with the code to get an access token, refresh token and
+    const res = await fetch("https://accounts.spotify.com/api/token", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: new URLSearchParams({
+        client_id: clientId,
+        grant_type: "authorization_code",
+        code,
+        redirect_uri: redirectUri,
+        code_verifier: codeVerifier,
+      }),
+    });
+
+    const json = await res.json();
+
+    // Store the token into local storage
+    localStorage.setItem("accesstoken", json.access_token);
+
+    // Clear the url search params
+    window.location.search = "";
+  }
+
+  handleRedirect() {
+    // Get the search parameters, if any from the url
+    const params = new URL(window.location).searchParams;
+    const code = params.get("code");
+    const urlState = params.get("state");
+
+    // If we were redirected from spotify, we should get 2 search parameters: code and state
+    if (code && urlState) {
+      // verify the state we got from the redirect url
+      if (sessionStorage.getItem("stateString") !== urlState) {
+        this.setState({ validState: false });
+      } else {
+        this.generateNewToken(code);
+      }
+    } else {
+      // Generate a state string and store it in sessionStorage so we can verify it after we get redirected back from spotify
+      const urlState = this.base64Encode(crypto.randomBytes(30));
+      sessionStorage.setItem("stateString", urlState);
+
+      // Generate code challenge
+      const codeVerifier = this.base64Encode(crypto.randomBytes(32));
+      sessionStorage.setItem("codeVerifier", codeVerifier);
+      const codeChallenge = this.base64Encode(this.sha256(codeVerifier));
+      // TODO: think about moving this into the constructor so we don't trigger extra rerenders (might need to move generating login link to constructor in this case)
       this.setState({
-        token: token,
+        urlState,
+        codeChallenge,
       });
     }
   }
@@ -40,7 +109,12 @@ class App extends React.Component {
     return (
       <div className="App">
         <header className="App-header">
-          {!this.state.token && (
+          {!this.state.validState && (
+            <div>
+              <p>ERROR: states do not match!</p>
+            </div>
+          )}
+          {this.state.validState && !this.state.tokenExists && (
             <div id="login">
               <h1>PopBubbles</h1>
               <p>
@@ -50,13 +124,19 @@ class App extends React.Component {
                 className="btn btn--loginApp-link"
                 href={`${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
                   "%20"
-                )}&response_type=token&show_dialog=true`}
+                )}&response_type=code&show_dialog=true&state=${
+                  this.state.urlState
+                }&code_challenge_method=S256&code_challenge=${
+                  this.state.codeChallenge
+                }`}
               >
                 Login to Spotify
               </a>
             </div>
           )}
-          {this.state.token && <BubblesList token={this.state.token} />}
+          {this.state.validState && this.state.tokenExists && (
+            <BubblesList token={this.token} />
+          )}
         </header>
       </div>
     );
